8900e9bb31086b2653463d19fc3fadcf267483c9
TAYLUNG <alefdutran@gmail.com>
2026-03-16 14:30:42 -0300

    Ajustes nos testes de inclusão, edição e remoção com verificações

--- cypress/e2e/ebac-exercicio/proj.cy.js
+++ cypress/e2e/ebac-exercicio/proj.cy.js
@@ -12,6 +12,8 @@ describe("Abrindo URL", () => {
     cy.get('input[type="tel"]').type("31982749178");
     // Adicionando
     cy.get(".adicionar").click();
+    // Veriricando elementos de preenchimento;
+    cy.contains("Taylung Dutra").should("be.visible");
   });
   it("Editando o contato da lista", () => {
     // editando...
@@ -20,10 +22,11 @@ describe("Abrindo URL", () => {
     cy.get('input[type="email"]').clear().type("pauloCy@gmail.com");
     cy.get('input[type="tel"]').clear().type("31982938291");
     cy.get('button[type="submit"]').click();
+    // Verificação: contato atualizado aparece
+    cy.contains("Paulo Santos Pereira").should("be.visible");
   });
   it("Deletando contato da lista", () => {
     cy.get(".delete").first().click();
-    cy.get(".delete").last().click();
-    cy.get(".delete").eq(2).click();
+    cy.contains("Paulo Santos Pereira").should("not.exist");
   });
 });
